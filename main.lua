a48fc60fca40dbadf17fd1a7a800d61e0d52817e
Rock48 <cquiglz@gmail.com>
2014-04-25 21:15:56 -0400

    First commit -- game concept

---
+++ main.lua
@@ -0,0 +1,17 @@
+require "lib/light"
+require "lib/postshader"
+class = require "middleclass"
+
+
+function love.load(arg)
+  
+  
+end
+
+function love.update(dt)
+  
+end
+
+function love.draw()
+  
+end
